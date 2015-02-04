0777af2f543f86238e1655b670e9fdab36427049
yindeng <yindeng@users.noreply.github.com>
2015-02-03 20:55:55 -0800

    Delete ui for changing import text file settings

--- inst/shinyDataApp/ui.r
+++ inst/shinyDataApp/ui.r
@@ -51,22 +51,7 @@ shinyUI(navbarPage(
                fileInput1('file', 'Add Text File',
                          accept=c('text/csv',
                                   'text/comma-separated-values,text/plain',
-                                  '.csv')),
-               tags$hr(),
-
-               conditionalPanel("output.uploadingTextFile==true",
-                                checkboxInput('header', 'Header', TRUE),
-                                radioButtons('sep', 'Separator',
-                                             c(Comma=',',
-                                               Semicolon=';',
-                                               Tab='\t'),
-                                             ','),
-                                radioButtons('quote', 'Quote',
-                                             c(None='',
-                                               'Double Quote'='"',
-                                               'Single Quote'="'"),
-                                             '"')
-                                )
+                                  '.csv'))
 
 
              ),
