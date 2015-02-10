bebe333e103fb6d1e20856eef8d13a885cffd992
yindeng <yindeng@users.noreply.github.com>
2015-02-10 09:43:40 -0800

    code cleanup

--- inst/shinyDataApp/sheetsCustomize.r
+++ inst/shinyDataApp/sheetsCustomize.r
@@ -1,45 +1,32 @@
-## Selecting plot element for customizaiton
-observe({
-  v <- input$customizeItem
-  isolate({
-    if(!isEmpty(v)){
-      currentSheet <- (projProperties[['activeSheet']])
-      if(!isEmpty(currentSheet)) sheetList[[currentSheet]][['dynamicProperties']][['customizeItem']] <<- v
-    }
-  })
-})
-observe({
-  updateInput[['customizeItem']]
-  currentSheet <- projProperties[['activeSheet']]
-  s <- if(!isEmpty(currentSheet)){
-    isolate(sheetList[[currentSheet]][['dynamicProperties']][['customizeItem']])
-  } else ''
-  updateTabsetPanel(session, 'customizeItem', selected=null2String(s))
-})
-
-## modify plotTitle and other text fields
-lapply(c('plotTitle', 'plotXlab', 'plotYlab'),
-       function(inputId){
-         assign(paste0('observer_', inputId, '_push'),
+## upsert sheet dynamicProperties
+lapply(list(list(inputId='customizeItem', inputType='tabsetPanel'),
+            list(inputId='plotTitle', inputType='text'),
+            list(inputId='plotXlab', inputType='text'),
+            list(inputId='plotYlab', inputType='text')),
+       function(x){
+         assign(paste0('observer_', x$inputId, '_push'),
                 observe({
-                  v <- input[[inputId]]
+                  v <- input[[x$inputId]]
                   isolate({
                     currentSheet <- (projProperties[['activeSheet']])
                     if(!isEmpty(currentSheet)){
-                      sheetList[[currentSheet]][['dynamicProperties']][[inputId]] <<- v
+                      if(are.vectors.different(v, sheetList[[currentSheet]][['dynamicProperties']][[x$inputId]])){
+                        sheetList[[currentSheet]][['dynamicProperties']][[x$inputId]] <<- v
+                      }
                     }
                   })
-
                 }),
                 sessionEnv)
-         assign(paste0('observer_', inputId, '_pull'),
+         assign(paste0('observer_', x$inputId, '_pull'),
                 observe({
-                  updateInput[[inputId]]
+                  updateInput[[x$inputId]]
                   currentSheet <- projProperties[['activeSheet']]
                   s <- if(!isEmpty(currentSheet)){
-                    isolate(sheetList[[currentSheet]][['dynamicProperties']][[inputId]])
+                    isolate(sheetList[[currentSheet]][['dynamicProperties']][[x$inputId]])
                   } else ''
-                  updateTextInput(session, inputId, value=null2String(s))
+                  switch(x$inputType,
+                         'text'=updateTextInput(session, x$inputId, value=null2String(s)),
+                         'tabsetPanel'=updateTabsetPanel(session, x$inputId, selected=null2String(s)))
                 }),
                 sessionEnv)
        })
@@ -70,8 +57,9 @@ lapply(list(list(inputId='textFamily', inputType='select'),
                           sheetList[[currentSheet]][['dynamicProperties']][[customizeItem]] <<- list()
                         }
                         if(x$inputId=='textColor' && !isEmpty(v)) v <- paste0("#", v)
-                        if(are.vectors.different(v, sheetList[[currentSheet]][['dynamicProperties']][[customizeItem]][[x$inputId]]))
+                        if(are.vectors.different(v, sheetList[[currentSheet]][['dynamicProperties']][[customizeItem]][[x$inputId]])){
                           sheetList[[currentSheet]][['dynamicProperties']][[customizeItem]][[x$inputId]] <<- v
+                        }
                       }
                     }
                   })
